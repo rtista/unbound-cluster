f71b76ea58cf726a75f4f000f01f279eb669ced4
João Santos <jsantos@e-goi.com>
2020-08-19 18:25:50 +0100

    Fix zone controller

--- api/controllers/zone.py
+++ api/controllers/zone.py
@@ -11,7 +11,7 @@ class ZoneController:
     Represents the Zone controller which handles zone CRUD requests.
     """
 
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, zone=None):
         """
         Handles GET requests.
 
@@ -19,8 +19,18 @@ class ZoneController:
         :param resp: The response object.
         :param zone: The DNS zone.
         """
+        # Get all records of zone
+        where = []
+
+        if zone:
+            where.append(f'zone = "{zone}"')
+
+        # Check for updated parameter
+        if 'updated' in req.params and str(req.params['updated']).isnumeric() and int(req.params['updated']) > 0:
+            where.append(f'updated > {int(req.params["updated"])}')
+
         # Select all zones from the database
-        queryset = self.dbconn.query(sqlalchemy.distinct(Record.zone)).all()
+        queryset = self.dbconn.query(Record.zone).filter(sqlalchemy.text(' AND '.join(where))).distinct().all() if where else self.dbconn.query(Record.zone).distinct().all()
 
         resp.media = { 'zones': [ zone for (zone, ) in queryset ] }
         resp.status = falcon.HTTP_200
