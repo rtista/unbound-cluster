13936a251368c4b766655dfa5d11bcba6646a481
rtista <workingtitus@gmail.com>
2020-08-29 02:09:27 +0100

    Reduced supported DNS records

--- models/record.py
+++ models/record.py
@@ -25,7 +25,7 @@ class Record(Base):
 
     zone = Column('zone', String(255), primary_key=True, nullable=False)
     resource = Column('resource', String(255), primary_key=True, nullable=False)
-    rtype = Column('type', Enum(*Config.VALID_RECORD_TYPES), primary_key=True, nullable=False)
+    rtype = Column('type', Enum(*Config.SUPPORTED_RECORD_TYPES), primary_key=True, nullable=False)
     ttl = Column('ttl', Integer, default=3600, nullable=False)
     rdata = Column('rdata', String(255), primary_key=True, nullable=False)
     created = Column('created', Integer, default=unixtime)
